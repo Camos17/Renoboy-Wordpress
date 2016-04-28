83d3bcc1ef5e845205a967c305fb0319c1310ad7
camos17 <camos8710@gmail.com>
2016-04-28 16:34:51 -0500

    Cambios estilo vista interna quienes confia

--- wp-content/themes/renoboytheme/quienes_confian.php
+++ wp-content/themes/renoboytheme/quienes_confian.php
@@ -111,7 +111,7 @@ Template Name: Quienes confian en nosotros
 							</div>
 						</div>
 						<div class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4 hidden-xs hidden-sm hidden-md col-md-4 col-md-offset-4 col-lg-2 btn-porque-confian">
-							<a class="col-xs-6 btn btn-block" href="#" title="">¿POR QUÉ <br> CONFIAN <br> EN RENOBOY? </a>
+							<!-- <a class="col-xs-6 btn btn-block" href="#" title=""> -->¿POR QUÉ <br> CONFIAN <br> EN RENOBOY?<!--  </a> -->
 						</div>
 						<div class="col-xs-12 footer-quienes-confian">
 							<p>Contamos con <strong>93 Centros de Servicio de la Red  de Distribución  Michelin  a nivel nacional</strong>, los cuales atienden la línea de camión y garantizan la presencia del servicio de reencauche de Renoboy. </p>
