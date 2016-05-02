ffb726cf723c645fb1703f3fde94886ab028002b
camos17 <camos8710@gmail.com>
2016-05-02 18:40:20 -0500

    Se inicia ajustes para el modal de producto

--- wp-content/themes/renoboytheme/buscadorserviciosreno.php
+++ wp-content/themes/renoboytheme/buscadorserviciosreno.php
@@ -354,22 +354,48 @@ Template Name: Buscador Servicios
 		<div class="modal fade modal-regional" id="modal-regional" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
 		  <div class="modal-dialog" role="document">
 		    <div class="modal-content contenido-modal-regional">
-		    	<div class="col-md-6 col-md-push-6 modal-header hidden-md-hidden-lg headline-modal-regional">
-		        	<button type="button" class="close cerrar-ayuda" data-dismiss="modal" aria-label="Close">
+		    	<div class="col-md-6 col-md-push-6 modal-header headline-modal-regional">
+		        	<button type="button" class="close cerrar-ayuda hidden-md hidden-lg" data-dismiss="modal" aria-label="Close">
 		        		<span aria-hidden="true">
 		        			<i class="ion-android-close"></i>
 		        		</span>
 		        	</button>
-		        	<h2 class="modal-title" id="myModalLabel">SPREA AXEL</h2>
+		        	<h2 class="modal-title col-xs-8" id="myModalLabel">REGIONAL</h2>
+		        	<div class="col-xs-4 col-sm-3 pull-right">
+	    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/REGIONAL.svg" alt="">
+					</div>
 		    	</div>
-		    	<div class="hidden-xs hidden-sm col-md-6 col-md-pull-6">
+		    	<div class="col-xs-12 col-md-6 col-sm-offset-3">
 		    		<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/5.jpg" alt="">		    		
 		    	</div>
 		    	<div class="col-xs-12 col-sm-12 col-sm-offset-0 col-md-12 col-lg-12 contenido-modal-regional2">
 		    		<div class="col-xs-12 contenido-modal-regional3">
-		    			<div class="col-sm-8 col-sm-offset-2 hidden-md hidden-lg">
-			    			<img class="img-responsive llanta-desplegada" src="<?php bloginfo('template_directory');?>/img/5.jpg" alt="">		    		
-			    		</div>
+
+		    			<div class="col-xs-12 logo-recamic">
+		    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/logo_recamic.jpg" alt="...">
+		    			</div>
+		    			<div class="col-xs-12 img-aplicacion">
+		    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionrecomendada.svg" alt="...">
+		    				<p></p>
+		    			</div>
+		    			<div class="col-xs-12 img-aplicacion">
+		    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionaceptada.svg" alt="...">
+		    				<p></p>
+		    			</div>
+
+
+			    		<div class="col-xs-12 texto-modal-regional">
+			    			<h3>carácteristicas y beneficios</h3>
+			    			<div class="col-xs-12 no-padding">
+			    				<hr>
+			    			</div>
+			    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			    			cillum dolore eu fugiat nulla pariatur.</p>
+			    		</div>	
+
 			    		<div class="col-sm-8 col-sm-offset-2 table-responsive no-padding">	
 							<table class="table table-hover">
 								<!-- <caption>Titulo Tabla</caption>
@@ -397,7 +423,25 @@ Template Name: Buscador Servicios
 								</tbody>
 							</table>
 						</div>
-						<div class="col-xs-12 col-sm-8 col-sm-offset-2 no-padding btn-compra-modal-regional1">
+
+						<div class="col-xs-12 texto-modal-regional2">
+							<div class="col-xs-12 texto-convenciones">
+								<img class="col-xs-1 no-padding" src="<?php bloginfo('template_directory');?>/img/dinero.svg" alt="">
+								<p class="col-xs-11">PRECIO: Consulte con su distribuidor.</p>
+							</div>
+							<div class="col-xs-12 texto-convenciones">
+								<img class="col-xs-1 no-padding" src="<?php bloginfo('template_directory');?>/img/documento.svg" alt="">
+								<p class="col-xs-11">INVENTARIO: Producto sujeto a disponibilidad.</p>
+							</div>
+							<div class="col-xs-12 texto-convenciones">
+								<img class="col-xs-1 no-padding" src="<?php bloginfo('template_directory');?>/img/check.svg" alt="">
+								<p class="col-xs-11">GARANTÍA Y CONDICIONES DE USO: Link a PDF.</p>
+							</div>
+						</div>
+
+
+
+						<!-- <div class="col-xs-12 col-sm-8 col-sm-offset-2 no-padding btn-compra-modal-regional1">
 							<button type="button" class="col-xs-12 no-padding btn btn-dafault">
 								<a href="#" title="">Tengo más de 10 Vehículos, Dónde comprar?</a>
 							</button>
@@ -406,17 +450,12 @@ Template Name: Buscador Servicios
 							<button type="button" class="col-xs-12 no-padding btn btn-dafault">
 								<a href="#" title="">Tengo menos de 10 Vehículos, Dónde comprar?</a>
 							</button>
-						</div>
+						</div> -->
 		    		</div>
 		    	</div>		    		
 		    	<div class="modal-footer ayuda-modal-footer">
 					<div class="col-xs-12 col-sm-12 no-padding">
-						<div class="col-xs-8 col-sm-5 col-sm-offset-4">
-							<p>REGIONAL</p>
-						</div>		    			
-		    			<div class="col-xs-4 col-sm-3 pull-right">
-		    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/REGIONAL.svg" alt="">
-						</div>
+						
 					</div>
 		        	<button type="button" class="btn btn-default cerrar-ayuda2 hidden-md hidden-lg" data-dismiss="modal">Cerrar</button>
 		    	</div>
