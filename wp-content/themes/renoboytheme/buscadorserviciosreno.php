ecb9a12484f66d4de203c4b976254a66cb92ed5d
camos17 <camos8710@gmail.com>
2016-05-05 18:38:11 -0500

    Cambios en el estilo del modal de producto de la vista de buscador de servicio

--- wp-content/themes/renoboytheme/buscadorserviciosreno.php
+++ wp-content/themes/renoboytheme/buscadorserviciosreno.php
@@ -114,7 +114,7 @@ Template Name: Buscador Servicios
 			</div>
 			<div class="col-xs-12 col-sm-12 col-md-7 cat-llantas">
 				<div class="col-xs-12 col-sm-5 col-sm-offset-1 col-md-6 col-md-offset-0 no-padding">
-					<a href="#" class="col-xs-6 cat-llanta" data-toggle="modal" data-target="#modal-regional">
+					<a href="#" class="col-xs-6 cat-llanta" data-toggle="modal" data-target="#modal-producto">
 						<div class="col-xs-12 no-padding cat-llanta-wrapper">
 							<img  class="img-responsive" src="<?php bloginfo('template_directory');?>/img/1.jpg" alt="">
 							<div class="col-xs-12 no-padding layer-cat-llanta">		
@@ -351,127 +351,113 @@ Template Name: Buscador Servicios
 		    </div>
 		  </div>
 		</div>
-		<div class="modal fade modal-regional" id="modal-regional" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		  <div class="modal-dialog" role="document">
-		    <div class="modal-content contenido-modal-regional">
-		    	<div class="col-sm-10 col-sm-offset-1 col-md-6 col-lg-5 modal-header headline-modal-regional">
-		        	<button type="button" class="close cerrar-ayuda hidden-md hidden-lg" data-dismiss="modal" aria-label="Close">
-		        		<span aria-hidden="true">
-		        			<i class="ion-android-close"></i>
-		        		</span>
-		        	</button>
-		        	<h2 class="modal-title col-xs-8" id="myModalLabel">REGIONAL</h2>
-		        	<div class="col-xs-4 col-sm-3 col-md-4 pull-right-1">
-	    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/REGIONAL.svg" alt="">
-					</div>
-					<div class="col-xs-12 col-sm-12 hidden-xs hidden-sm imagen-llanta">
-		    			<img class="img-responsive va" src="<?php bloginfo('template_directory');?>/img/5.jpg" alt="">	   		
-		    		</div>
-		    	</div>
-    
-		    	<div class="modal-body col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-0 col-lg-7">
-		    		<div class="col-xs-12 hidden-md hidden-lg imagen-llanta no-padding">
-		    			<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/5.jpg" alt="">		    		
-		    		</div>	    		
-		    		<div class="col-xs-12 col-sm-12 col-sm-offset-0 col-md-12 col-lg-12 contenido-modal-regional2 no-padding">
-		    			<div class="col-xs-12 contenido-modal-regional3">
-
-		    				<button type="button" class="close cerrar-ayuda hidden-xs hidden-sm" data-dismiss="modal" aria-label="Close">
-				        		<span aria-hidden="true">
-				        			<i class="ion-android-close"></i>
-				        		</span>
-				        	</button>
-
-			    			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-5 col-lg-offset-1 logo-recamic">
-			    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/logo_recamic.jpg" alt="...">
-			    			</div>
-			    			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-5 img-aplicacion">
-			    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionrecomendada.svg" alt="...">
-			    				<p class="va">aplicación <br> recomendada</p>
-			    			</div>
-			    			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-6 col-lg-5 img-aplicacion">
-			    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionaceptada.svg" alt="...">
-			    				<p class="va">aplicación <br> aceptada</p>
-			    			</div>
-
-
-				    		<div class="col-xs-12 texto-modal-regional">
-				    			<h3>carácteristicas y beneficios</h3>
-				    			<div class="col-xs-12 no-padding">
-				    				<hr>
-				    			</div>
-				    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				    			cillum dolore eu fugiat nulla pariatur.</p>
-				    		</div>	
-
-				    		<div class="col-sm-10 col-sm-offset-1 table-responsive tabla-modal-producto no-padding">	
-								<table class="table table-hover">
-									<thead>
-										<tr>
-											<th>  DIMENSIÓN </th>
-											<th>ANCHO DE BANDA (mm)</th>
-											<th>PROFUNDIDAD (mm)</th>
-											<th>CORRESPONDENCIA (CARCASA)</th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-										</tr>
-										<tr>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-										</tr>
-										<tr>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-											<td>data</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-
-							<div class="col-xs-12 texto-modal-regional2">
-								<div class="col-xs-12 texto-convenciones">
-									<i class="col-xs-1 no-padding fa fa-usd" aria-hidden="true"></i>
-									<p class="col-xs-11">PRECIO: Consulte con su distribuidor.</p>
+		
+		<div id="modal-producto" class="modal fade modal-producto" tabindex="-1" role="dialog" aria-labelledby="gridModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-xl" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="container-fluid no-padding">
+							<div class="col-xs-12 modal-producto-wrapper no-padding">
+								<button type="button" class="close btn-cerrar-modal" data-dismiss="modal" aria-label="Close">
+									<i class="fa fa-times-circle" aria-hidden="true"></i>
+								</button>
+								
+								<div class="col-xs-12 col-md-4 no-padding headline-modal-producto">
+									<div class="col-xs-12 no-padding headline-modal-producto2">
+										<h2 class="modal-title col-xs-8">REGIONAL</h2>
+							        	<div class="col-xs-4 col-sm-3 col-md-4 pull-right-1 icono-header">
+						    				<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/REGIONAL.svg" alt="">
+										</div>
+									</div>
+									<div class="col-xs-12 imagen-llanta">
+										<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/5.jpg" alt="">
+									</div>	
 								</div>
-								<div class="col-xs-12 texto-convenciones">
-									<i class="col-xs-1 no-padding fa fa-file-text icono-documento" aria-hidden="true"></i>
-									<p class="col-xs-11">INVENTARIO: Producto sujeto a disponibilidad.</p>
+
+								<div class="col-xs-12 no-padding contenido-modal-producto">
+									<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-5 col-lg-offset-1 logo-recamic">
+			    						<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/logo_recamic.jpg" alt="...">
+			    					</div>
+			    					<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-0 col-lg-4 img-aplicacion">
+			    						<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionrecomendada.svg" alt="...">
+			    						<p class="va">aplicación <br> recomendada</p>
+			    					</div>
+			    					<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-6 col-lg-4 img-aplicacion">
+			    						<img class="img-responsive" src="<?php bloginfo('template_directory');?>/img/aplicacionaceptada.svg" alt="...">
+			    						<p class="va">aplicación <br> aceptada</p>
+			    					</div>
+			    					<div class="col-xs-12 texto-modal-producto">
+				    					<h3>carácteristicas y beneficios</h3>
+				    					<div class="col-xs-12 no-padding">
+				    						<hr>
+				    					</div>
+						    			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						    			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						    			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						    			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						    			cillum dolore eu fugiat nulla pariatur.</p>
+					    			</div>
+
+					    			<div class="col-sm-10 col-sm-offset-1 table-responsive tabla-modal-producto no-padding">	
+										<table class="table table-hover">
+											<thead>
+												<tr>
+													<th>  DIMENSIÓN </th>
+													<th>ANCHO DE BANDA (mm)</th>
+													<th>PROFUNDIDAD (mm)</th>
+													<th>CORRESPONDENCIA (CARCASA)</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+												</tr>
+												<tr>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+												</tr>
+												<tr>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+													<td>data</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+
+									<div class="col-xs-12 texto-modal-producto2">
+										<div class="col-xs-12 texto-convenciones">
+											<i class="col-xs-1 no-padding fa fa-usd" aria-hidden="true"></i>
+											<p class="col-xs-11">PRECIO: Consulte con su distribuidor.</p>
+										</div>
+										<div class="col-xs-12 texto-convenciones">
+											<i class="col-xs-1 no-padding fa fa-file-text icono-documento" aria-hidden="true"></i>
+											<p class="col-xs-11">INVENTARIO: Producto sujeto a disponibilidad.</p>
+										</div>
+										<div class="col-xs-12 texto-convenciones">
+											<i class="col-xs-1 no-padding fa fa-check-circle" aria-hidden="true"></i>
+											<p class="col-xs-11">GARANTÍA Y CONDICIONES DE USO: Link a PDF.</p>
+										</div>
+									</div>
+
+									<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-7 col-md-offset-4 col-lg-5 col-lg-offset-6 btn-modal-producto-comprar">
+										<a class="col-xs-12 btn btn-block no-padding" href="#" title="">
+											<img class="col-xs-2 no-padding" src="<?php bloginfo('template_directory');?>/img/carritodecompras.svg" alt="...">
+											<p>Quiero Comprar</p>
+										</a>
+									</div>
 								</div>
-								<div class="col-xs-12 texto-convenciones">
-									<i class="col-xs-1 no-padding fa fa-check-circle" aria-hidden="true"></i>
-									<p class="col-xs-11">GARANTÍA Y CONDICIONES DE USO: Link a PDF.</p>
-								</div>
-							</div>
-
-							<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-7 col-md-offset-4 col-lg-5 col-lg-offset-6 btn-modal-regional-comprar">
-								<a class="col-xs-12 btn btn-block no-padding" href="#" title="">
-									<img class="col-xs-2 no-padding" src="<?php bloginfo('template_directory');?>/img/carritodecompras.svg" alt="...">
-									<p>Quiero Comprar</p>
-								</a>
-							</div>
-
-
-						</div>
-		    		</div>
-		    	</div>
-		    	<div class="modal-footer ayuda-modal-footer hidden-md">
-					<button type="button" class="btn btn-default cerrar-ayuda2 hidden-md hidden-lg" data-dismiss="modal">Cerrar</button>
-		    	</div>	    		
-		    	
-		    </div>
-		  </div>
+							</div>	
+						</div>							
+					</div>					
+				</div>
+			</div>
 		</div>
 
 		<?php include "menu-fijo.php";?>
