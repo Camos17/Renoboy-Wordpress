948a26210ee7cfa882e6ef26a66141244779ee2d
camos17 <camos8710@gmail.com>
2016-05-10 17:57:49 -0500

    Se incia conexion de la BD de reno con la vista de productos

---
+++ wp-content/themes/renoboytheme/noticiasdesplegadas.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Noticias Desplegadas
+*/
+?>
+
+	<?php include "header.php";?>
+
+		
+		<div class="col-xs-12 no-padding content-wrapper noticias">
+			<div class="hidden-xs col-sm-5 col-sm-offset-1 col-md-4 img-noticia-desplegada">
+				<img class="img-responsive" src="http://placehold.it/225x385" alt="">	
+			</div>
+			<div class="col-xs-12 col-sm-5 noticia-desplegada">
+				<h2>Tu llanta nueva de nuevo</h2>
+				<div class="col-xs-12 col-sm-12 separador">
+					<hr>	
+				</div>
+				<div class="col-xs-12 col-sm-12 no-padding wrapper-noticia-desplegada">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.	<br><br>
+					</p>
+				</div>
+				<div class="col-xs-12 col-sm-12 no-padding btn-regreso-noticia">
+					<a href="noticias.php"><i class="fa fa-chevron-circle-left"></i>Noticias de Interés</a>
+				</div>			
+			</div>
+		</div>
+		
+		<?php include "menu-fijo.php";?>
+
+		
+		<?php include "footer.php";?>
+
+	</body>	
+</html>	
+
